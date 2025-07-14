17dc156b8f4ecb0a28c929f3ecacf96ed122e072
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-07-14 15:33:11 -0300

    Exibindo a cotação do dólar

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,5 @@
+import imprimeCotacao from "./imprimeCotacao.js";
+
 const graficoDolar = document.getElementById('graficoDolar');
 const graficoParaDolar = new Chart(graficoDolar, {
   type: 'line',
@@ -11,15 +13,17 @@ const graficoParaDolar = new Chart(graficoDolar, {
   },
 });
 
-setInterval(() => conectaAPI(), 5000);
-async function conectaAPI() {
-  const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-  const conectaTraduzido = await conecta.json();
-  console.log(conectaTraduzido);
-  let tempo = geraHorario();
-  let valor = conectaTraduzido.USDBRL.ask;
-  adicionarDados(graficoParaDolar, tempo, valor);
-};
+// setInterval(() => conectaAPI(), 5000);
+
+// async function conectaAPI() {
+//   const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
+//   const conectaTraduzido = await conecta.json();
+//   console.log(conectaTraduzido);
+//   let tempo = geraHorario();
+//   let valor = conectaTraduzido.USDBRL.ask;
+//   adicionarDados(graficoParaDolar, tempo, valor);
+//   imprimeCotacao("dolar", valor);
+// };
 
 function geraHorario() {
   let data = new Date();
@@ -38,3 +42,12 @@ function adicionarDados(grafico, legenda, dados) {
   grafico.update();
 };
 
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", evento => {
+  let tempo = geraHorario();
+  let valor = evento.data.ask;
+  imprimeCotacao("dolar", valor);
+  adicionarDados(graficoParaDolar, tempo, valor);
+});
