936f157193f9b612010974a746c6058fe7aa3e9f
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-07-14 15:00:26 -0300

    Criando o gráfico com dados dinâmicos em tempo real para o dólar

---
+++ script/scripts.js
@@ -0,0 +1,40 @@
+const graficoDolar = document.getElementById('graficoDolar');
+const graficoParaDolar = new Chart(graficoDolar, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Dólar',
+      data: [],
+      borderWidth: 1
+    }]
+  },
+});
+
+setInterval(() => conectaAPI(), 5000);
+async function conectaAPI() {
+  const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
+  const conectaTraduzido = await conecta.json();
+  console.log(conectaTraduzido);
+  let tempo = geraHorario();
+  let valor = conectaTraduzido.USDBRL.ask;
+  adicionarDados(graficoParaDolar, tempo, valor);
+};
+
+function geraHorario() {
+  let data = new Date();
+  let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+  // console.log(data);
+  console.log(horario);
+  return horario;
+};
+
+function adicionarDados(grafico, legenda, dados) {
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach((dataset) => {
+    dataset.data.push(dados);
+  });
+
+  grafico.update();
+};
+
